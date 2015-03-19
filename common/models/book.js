eac6835c57db1291561cfb38fe25807e500cfb90
Puneet Goyal <pungoyal@gmail.com>
2015-03-19 09:12:26 +0530

    adding bunyan and a before remote

--- common/models/book.js
+++ common/models/book.js
@@ -1,3 +1,9 @@
-module.exports = function(Book) {
+var bunyan = require('bunyan')
+, log = bunyan.createLogger({name: 'Book'});
 
+module.exports = function(Book) {
+	Book.beforeRemote('*', function (ctx, model, next) {
+		log.info({a: 'b'}, 'Updated reviewer details');
+		next();
+	});
 };
